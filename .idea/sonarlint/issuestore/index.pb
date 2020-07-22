870dcbbe8f5dc808e71c1fad02f6133850d5d6d3
lccarelli <carelli.lc@gmail.com>
2020-07-22 12:16:57 -0300

    add functions structures

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+R
+"src/playground/JavaPlayground.java,4/5/452f55271b881423d9f542a4d3dd313e66be03ed
+T
+$src/playground/ScalaPlayground.scala,6/a/6adede57e6dee6c7ad4866320b79e492581b2281
+c
+3src/lectures/part1basics/ValuesVariablesTypes.scala,d/b/db50f1ac6b0b0de2c794629ab897dfca824f20f4
+Z
+*src/lectures/part1basics/Expressions.scala,5/9/597463b4265b4e7dbab2383e9b2e611b68340811
+X
+(src/lectures/part1basics/Functions.scala,e/8/e8270175992b56e52b86456146ce6e5b1abec8c4
--- src/lectures/part1basics/Functions.scala
+++ src/lectures/part1basics/Functions.scala
@@ -2,7 +2,28 @@ package lectures.part1basics
 
 object Functions extends App {
 
-  def aFunction(a: String, b: Int): String =
+  def aFunction(a: String, b: Int): String = {
     a + " " + b
+  }
+    println(aFunction("Hello",13))
+
+  def aParameterLessFunction():Int = 42
+  println(aParameterLessFunction())
+
+  def aRepeatedFunction(aString:String, n:Int): String ={
+    if (n == 1) aString
+    else aString + aRepeatedFunction(aString, n-1)
+
+    //when you need loops, use recursion
+  }
+  println(aRepeatedFunction("Hello", 3))
+
+  def aFunctionWithSideEffects(aString: String):Unit = println(aString)
+
+  def aBigFunction(n:Int):Int = {
+    def aSmallerFunction(a:Int, b:Int): Int =a + b
+    aSmallerFunction(n, n-1)
+  }
+
 
 }
