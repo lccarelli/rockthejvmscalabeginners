8e4b59a245c4794e06837b3fc828efca892eb1a2
lccarelli <carelli.lc@gmail.com>
2020-07-25 21:48:04 -0300

    functions && recursion

--- src/lectures/part1basics/Functions.scala
+++ src/lectures/part1basics/Functions.scala
@@ -24,6 +24,37 @@ object Functions extends App {
     def aSmallerFunction(a:Int, b:Int): Int =a + b
     aSmallerFunction(n, n-1)
   }
+  /*
+  * 1. A greeting function(name, age) => "Hi, my name is $name and I am $age years old"
+  * 2. Factorial function 1 * 2 * 3 * .. * n
+  * 3. A Fibonacci function
+  *     f(1) = 1
+  *     f(2) = 1
+  *     f(n) = f(n - 1) + f(n - 2)
+  * 4. Test if a number is prime
+  * */
 
+  def aGreeting(aName: String, aAge:Int):String =
+    "Hi, my name is " + aName + " and I am " + aAge + " years old"
+  println(aGreeting("Laura", 34))
 
+  def aFactorial(n:Int): Int =
+    if (n <= 0) 1
+    else n * aFactorial(n-1)
+  println(aFactorial(5))
+
+  def aFibonacci(n:Int): Int =
+    if(n <= 2) 1
+    else aFibonacci(n-1) + aFibonacci(n-2)
+  println(aFibonacci(8))
+
+  def isPrime(n: Int): Boolean = {
+    def isPrimeUntil(t:Int): Boolean =
+      if(t <= 1) true
+      else n % t != 0 && isPrimeUntil(t-1)
+    isPrimeUntil(n/2)
+  }
+  println(isPrime(37))
+  //println(isPrime(2003))
+  //println(isPrime(37 * 17))
 }
